7befc0aa3feb6151ae1d934bd7871a9e0a515a46
Guilherme da Rocha <guis.darocha@gmail.com>
2023-07-12 20:15:07 -0300

    Adicionado estrutura html na area de perguntas/FAQ

---
+++ clinica-salomao/src/components/Perguntas/Perguntas.tsx
@@ -0,0 +1,69 @@
+
+
+const Perguntas = () => {
+    return (
+    <>
+    <div className="cointener">
+        <h3>perguntas frequentes</h3>
+            <div>
+                <div className="pergunta">
+                    <p>Preciso fazer uma avaliação?</p>
+                </div>
+                <div className="resposta">
+                    <p>Sim, pois cada cliente tem uma indicação e anatomia distinta. Os tratamentos variam de acordo com o que vc deseja de resultado e indicação do profissional</p>
+                </div>
+            </div>
+            <div>
+                <div className="pergunta">
+                    <p></p>
+                </div>
+                <div className="resposta">
+                    <p></p>
+                </div>
+            </div>
+            <div>
+                <div className="pergunta">
+                    <p></p>
+                </div>
+                <div className="resposta">
+                    <p></p>
+                </div>
+            </div>
+            <div>
+                <div className="pergunta">
+                    <p></p>
+                </div>
+                <div className="resposta">
+                    <p></p>
+                </div>
+            </div>
+            <div>
+                <div className="pergunta">
+                    <p></p>
+                </div>
+                <div className="resposta">
+                    <p></p>
+                </div>
+            </div>
+            <div>
+                <div className="pergunta">
+                    <p></p>
+                </div>
+                <div className="resposta">
+                    <p></p>
+                </div>
+            </div>
+            <div>
+                <div className="pergunta">
+                    <p></p>
+                </div>
+                <div className="resposta">
+                    <p></p>
+                </div>
+            </div>
+    </div>
+    </>
+    )
+}
+
+export default Perguntas
