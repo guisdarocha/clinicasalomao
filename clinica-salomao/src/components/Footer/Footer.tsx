cf57a74b8f6c1ba32167ab474eda3d7f5df00316
Guilherme da Rocha <guis.darocha@gmail.com>
2023-07-12 20:33:15 -0300

    Estrutura HTML finalizada

--- clinica-salomao/src/components/Footer/Footer.tsx
+++ clinica-salomao/src/components/Footer/Footer.tsx
@@ -1,9 +1,27 @@
-
+import pin from '../../assets/pin.svg'
+import whats from '../../assets/whats_footer.svg'
+import instagram from '../../assets/instagram.svg'
 
 const Footer = () => {
     return (
     <>
-    <div className="container"></div>
+    <div className="container">
+        <div>
+            <img src={pin} alt="" />
+            <p>Av. Paulista 2006 3°andar -SP</p>
+        </div>
+        <div>
+            <img src={whats} alt="" />
+            <p>(11) 99717-3432</p>
+        </div>
+        <div>
+            <img src={instagram} alt="" />
+            <p>@esteticasalomao</p>
+        </div>
+        <div>
+            <p>CNPJ: 41.325.545/0001-55</p>
+        </div>
+    </div>
     </>
     )
 }
