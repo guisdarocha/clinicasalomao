bb8f5bebe4273e18c67ca02e1a9a4b439f2396b2
Guilherme da Rocha <guis.darocha@gmail.com>
2023-07-17 09:54:03 -0300

    responsividade finalizada do componente Metodo

--- clinica-salomao/src/components/Metodo/Metodo.tsx
+++ clinica-salomao/src/components/Metodo/Metodo.tsx
@@ -4,23 +4,30 @@ import metodo from '../../assets/metodo.png'
 const Metodo = () => {
     return (
     <>
-    <div className='grid grid-cols-2 bg-creme pt-[4rem]'>
-        <h3 className='text-[3.1rem] font-belleza text-vermelho-madeira text-left leading-tight'>Qual o método utilizado ?</h3>
-        <ul className='col-start-1 list-disc text-[1.8rem] font-belleza text-vermelho-madeira'>
-            <li className='text'>Muito simples, sem cirurgia e sem necessidade de repouso.</li>
-            <li>Técnicas e protocolos inovadores especialmente para você
-            acabar com tudo que mais te incomoda de forma personalizada!</li>
-        </ul>
-        <img src={metodo} alt="" className='col-start-2'/>
-        <ul className='col-span-2 list-disc text-[1.8rem] font-belleza text-vermelho-madeira'>
-            <li>Método seguro, minimamente invasivo sem necessidade de um processo
-            de recuperação, pois não se trata de uma cirurgia.</li>
-            <li>Protocolos mundialmente conhecidos pela eficiência em resultados nas clientes</li>
-        </ul>
-        <button className='col-span-2'>
-            <img src={whats} alt=""/>
-            <span>Quero fazer uma avaliação gratuita</span>
-        </button>
+    <div className="bg-creme flex justify-center items-center mx-auto">
+        
+        <div className='grid grid-cols-2 flex justify-center items-center max-w-[1080px] pt-[4rem]'>
+            <h3 className='text-[3.1rem] px-[3rem] py-[4rem] w-[35rem] font-belleza text-vermelho-madeira text-left uppercase leading-tight'>Qual o método utilizado ?</h3>
+            <ul className='col-start-1 list-disc  text-[1.8rem] px-[3rem] font-belleza text-vermelho-madeira'>
+                <li className='py-[2rem]'>Muito simples, sem cirurgia e sem necessidade de repouso.</li>
+                <li>Técnicas e protocolos inovadores especialmente para você
+                acabar com tudo que mais te incomoda de forma personalizada!</li>
+            </ul>
+            <img src={metodo} alt="" className='col-start-2'/>
+            <ul className='col-span-2 list-disc py-[3rem] text-[1.8rem] px-[3rem] font-belleza text-left text-vermelho-madeira'>
+                <li className='py-[2rem]'>Método seguro, minimamente invasivo sem necessidade de um processo
+                de recuperação, pois não se trata de uma cirurgia.</li>
+                <li>Protocolos mundialmente conhecidos pela eficiência em resultados nas clientes</li>
+            </ul>
+
+            <div className='col-span-2 flex justify-center '>
+                    <button className='flex bg-gradient-to-r from-whats-escuro via-whats to-whats-escuro border-4 border-branco drop-shadow-lg cursor-pointer px-[4rem] pt-[2rem] pb-[1rem] rounded-[8rem]'>
+                        <span className='text-[2.1rem] font-belleza w-[43rem] text-center drop-shadow-lg text-branco leading-tight uppercase'>Quero fazer uma avaliação gratuita</span>
+                        <img src={whats} alt="" className='animate-bounce pl-[1rem] self-center '/>
+                    </button>
+            </div>
+        </div>
+
     </div>
     </>
     )
