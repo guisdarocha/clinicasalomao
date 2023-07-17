6a94dc043bd0434719ef449a613a0a437c4493cc
Guilherme da Rocha <guis.darocha@gmail.com>
2023-07-17 10:41:30 -0300

    inicado estilização do componente Processos

--- clinica-salomao/src/components/Processos/Processos.tsx
+++ clinica-salomao/src/components/Processos/Processos.tsx
@@ -8,36 +8,48 @@ import codigoBarras from '../../assets/codigoBarras.png'
 const Processos = () => {
     return (
     <>
-    <div className="container">
-        <h3>Você está com dúvida?</h3>
-        <h4>Então veja os resultados abaixo de nossas clientes, tenho certeza que
-        você vai Amar cada resultado e dar seu primeiro passo para seu rejuvenescimento!</h4>
-        <div>
-            <img src={tresUm} alt="" />
-            <p>Bigode Chines</p>
-            <p>Marionete</p>
-            <p>Papada</p>
+    <div className="flex justify-center items-center mx-auto">
+        
+        <div className='grid grid-cols-2'>
+
+            <div className="col-span-2 justify-center items-center mx-auto py-[4rem] px-[4rem] ">
+                <h3 className='text-[3.1rem] font-belleza text-center text-vermelho-madeira uppercase'>Você está com dúvida?</h3>
+                <h4 className='text-[2.3rem]  w-[50rem] pt-[2rem] font-belleza text-center text-vermelho-madeira leading-tight'>Então veja os resultados abaixo de nossas clientes, tenho certeza que
+                você vai Amar cada resultado e dar seu primeiro passo para seu rejuvenescimento!</h4>
+            </div>
+
+            <div className='col-start-1'>
+                <p>Bigode Chines</p>
+                <p>Marionete</p>
+                <p>Papada</p>
+            </div>
+            <div>
+                <img src={tresUm} alt="" className='col-start-2'/>
+            <div>
+
+                    <p>Papada</p>
+                    <img src={papada} alt="" />
+                </div>
+                <div>
+                    <img src={bigodeChines} alt="" />
+                    <p>Bigode Chines</p>
+                </div>
+                <div>
+                    <p>Labial</p>
+                    <img src={labial} alt="" />
+                </div>
+                <div>
+                    <img src={codigoBarras} alt="" />
+                    <p>Código de barras</p>
+                </div>
+                <div>
+                    <p>Papada</p>
+                    <img src={papadaDois} alt="" />
+                </div>
+            </div>
         </div>
-        <div>
-            <p>Papada</p>
-            <img src={papada} alt="" />
-        </div>
-        <div>
-            <img src={bigodeChines} alt="" />
-            <p>Bigode Chines</p>
-        </div>
-        <div>
-            <p>Labial</p>
-            <img src={labial} alt="" />
-        </div>
-        <div>
-            <img src={codigoBarras} alt="" />
-            <p>Código de barras</p>
-        </div>
-        <div>
-            <p>Papada</p>
-            <img src={papadaDois} alt="" />
-        </div>
+
+
     </div>
     </>
     )
