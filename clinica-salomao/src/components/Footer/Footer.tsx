c81b97384c0d0d567eb0746227e7927116a8fad6
Guilherme da Rocha <guis.darocha@gmail.com>
2023-07-20 20:08:57 -0300

    Finalizando componente footer - estilização e responsividade

--- clinica-salomao/src/components/Footer/Footer.tsx
+++ clinica-salomao/src/components/Footer/Footer.tsx
@@ -5,21 +5,37 @@ import instagram from '../../assets/instagram.svg'
 const Footer = () => {
     return (
     <>
-    <div className="container">
-        <div>
-            <img src={pin} alt="" />
-            <p>Av. Paulista 2006 3°andar -SP</p>
-        </div>
-        <div>
-            <img src={whats} alt="" />
-            <p>(11) 99717-3432</p>
-        </div>
-        <div>
-            <img src={instagram} alt="" />
-            <p>@esteticasalomao</p>
-        </div>
-        <div>
-            <p>CNPJ: 41.325.545/0001-55</p>
+    <div className=" bg-creme-claro">
+
+            <div className="flex justify-center mx-auto max-w-[1080px] pb-[2rem] ">
+                <div className="w-[720px] lg:w-[780px] h-[0.1rem] bg-vermelho-madeira "></div>
+            </div>
+
+        <div className="flex justify-center mx-auto max-w-[1080px] pb-[3rem] font-belleza">
+          
+            <div className='flex justify-center items-center pl-[0.3rem]'> 
+                <div className="px-[0.7rem]">
+                    <img src={pin} alt="" className='w-[2.2rem] h-auto '/>
+                </div>
+                <p className='text-[1rem] w-[9.3rem] text-vermelho-madeira leading-tight '>Av. Paulista 2006 3°andar -SP</p>
+            </div>
+
+            <div className='flex justify-center items-center pl-[0.3rem]'>
+                <div className="px-[0.7rem]">
+                    <img src={whats} alt="" className='w-[2.2rem] h-auto '/>
+                </div>
+                <p className='text-[1rem] w-[9.3rem] text-vermelho-madeira leading-tight '>(11) 99717-3432</p>
+            </div>
+
+            <div className='flex justify-center items-center pl-[0.3rem]'>
+                <div className="px-[0.7rem]">
+                    <img src={instagram} alt="" className='w-[2.2rem] h-auto '/>
+                </div>
+                <p className='text-[1rem] w-[9.3rem] text-vermelho-madeira leading-tight '>@esteticasalomao</p>
+            </div>
+            <div>
+                <p className='text-[1rem] w-[9.3rem] text-vermelho-madeira leading-tight '>CNPJ: 41.325.545/0001-55</p>
+            </div>
         </div>
     </div>
     </>
