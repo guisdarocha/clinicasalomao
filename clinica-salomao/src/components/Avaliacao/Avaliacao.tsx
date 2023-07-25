b8787a3d422bba9b1aa2b5f46866e625e8eee253
Guilherme da Rocha <guis.darocha@gmail.com>
2023-07-25 20:44:38 -0300

    Finalizado alterações componente avaliacao

--- clinica-salomao/src/components/Avaliacao/Avaliacao.tsx
+++ clinica-salomao/src/components/Avaliacao/Avaliacao.tsx
@@ -4,27 +4,33 @@ import whats from '../../assets/whats.svg'
 const Avaliacao = () => {
     return (
     <>
-    <div className='bg-vermelho-madeira drop-shadow-lg'>
+    <div className='bg-vermelho-madeira drop-shadow-lg h-auto'>
 
         <div className='flex justify-center items-center mx-auto max-w-[1080px] '>
+            <div className='grid grid-col-2'>
 
-            <div className='grid grid-cols-2'>
-                <div className="justify-center items-center mx-auto pl-[3rem] pt-[5rem]">
-                    <p className='text-[1.8rem] text-creme font-belleza leading-tight'>Através de uma avaliação personalizada, individual um protocolo será.</p>
-                    <p className='text-[1.8rem] pt-[2rem] text-creme font-belleza leading-tight'>Montado especialmente para você. Nosso profissional irá lhe apresentar o melhor tratamento, para que você tenha resultado que sempre desejou e saia.</p>
-                    <p className='text-[1.8rem] pt-[2rem] text-creme font-belleza leading-tight'>Maravilhada com sua autoestima nas alturas. Faça inveja para suas amigas e fique 10 anos mais jovem.</p>
-                    
-                    <div className='pt-[3rem]'>
-                    <a href='https://wa.me/5511997173432?text=Gostaria de saber mais sobre a Estética Salomão'>
-                        <button className='flex px-[0.5rem] justify-center items-center mx-auto bg-gradient-to-r from-whats-escuro via-whats  to-whats-escuro py-[1rem] border-2 border-branco drop-shadow-lg cursor-pointer rounded-[8rem] hover:shadow-dentro hover:shadow-creme'>
-                            <span className='text-[1.2rem] font-belleza  pl-[0.8rem] text-center drop-shadow-lg text-branco leading-tight uppercase'>Quero iniciar agora meu tratamento</span>
-                            <img src={whats} alt="" className=' pl-[0.5rem] pr-[0.3rem] self-center w-[60px] '/>
-                        </button>
-                    </a>    
-                    </div>
-                </div>
-                <div className='flex justify-end'>
-                    <img src={senhora} alt="" className='' />
+            
+                <div className='cols-span-2'>
+
+                        <div className='flex justify-center items-center mx-auto'>
+
+                            <img src={senhora} alt="" className='max-w-[68rem] max-h-[31rem]' />
+                        </div>
+        
+                        <div className="text-center justify-center items-center mx-auto  pt-[5rem] w-[53rem]">
+                            <p className='text-[2.5rem] text-creme-claro font-mont '>Através de uma avaliação personalizada e individual, um protocolo será montado especialmente para você.</p>
+                            <p className='text-[2.5rem] pt-[2rem] text-creme-claro font-mont '>Nosso profissional irá lhe apresentar o melhor tratamento, para que você tenha resultado que sempre sonhou e saia com a sua autoestima nas alturas.</p>
+                            <p className='text-[2.5rem] pt-[2rem] text-creme-claro font-mont '>Faça inveja para suas amigas e fique 10 anos mais jovem!</p>
+                        </div>
+
+                                <div className='pt-[5rem] -mb-[2rem] flex justify-center '>
+                                    <a href='https://wa.me/5511997173432?text=Gostaria de saber mais sobre a Estética Salomão'>
+                                            <button className='flex justify-center items-center mx-auto bg-gradient-to-r from-whats-escuro via-whats to-whats-escuro border-4 border-branco drop-shadow-lg cursor-pointer px-[4rem] pt-[1rem] pb-[1rem] rounded-[8rem] hover:shadow-dentro hover:shadow-creme hover:transition-all hover:ease-out hover:duration-700'>
+                                                <span className='text-[2.1rem] font-mont font-extrabold w-[48rem] text-center drop-shadow-lg text-branco leading-tight uppercase hover:text-[2.2rem] hover:w-[51rem] hover:transition-all hover:ease-out hover:duration-700'>Quero fazer uma avaliação gratuita</span>
+                                                <img src={whats} alt="" className='pl-[1rem] self-center '/>
+                                            </button>
+                                    </a>
+                                </div>
                 </div>
             </div>
 
