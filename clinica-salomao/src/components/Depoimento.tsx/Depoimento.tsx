faae8d8d4a0af79fbce451d289ee913e9f76542c
Guilherme da Rocha <guis.darocha@gmail.com>
2023-07-25 21:52:12 -0300

    Finalizado alterações componente perguntas

--- clinica-salomao/src/components/Depoimento.tsx/Depoimento.tsx
+++ clinica-salomao/src/components/Depoimento.tsx/Depoimento.tsx
@@ -8,7 +8,7 @@ const Depoimento = () => {
         <div className="grid grid-cols-2 max-w-[1080px] py-[4rem]">
             <div className=" font-belleza text-creme ml-[5rem] w-[20rem]">
                 <h3 className='text-[3.5rem] leading-tight uppercase'>Veja o que nossos clientes relatam sobre nós</h3>
-                <h5 className='text-[1.8rem] leading-tight'>Passe para o lado e veja outras avaliações</h5>
+                <h5 className='text-[1.6rem] mt-[1.5rem] font-mont leading-tight'>Passe para o lado e veja outras avaliações</h5>
             </div>      
             
             <div className='hidden absolute lg:left-[10rem] lg:top-[898rem] xl:left-[20rem] xl:top-[894rem]'>
--- clinica-salomao/src/components/Perguntas/Perguntas.tsx
+++ clinica-salomao/src/components/Perguntas/Perguntas.tsx
@@ -35,26 +35,26 @@ import whats from '../../assets/whats.svg'
     <Fragment>
         <div className="bg-creme-claro pt-[5rem]">
             <div className="max-w-[700px] flex justify-start mx-auto pb-[2rem]">
-              <h3 className="text-[3.5rem] text-vermelho-madeira font-belleza leading-tight uppercase max-w-[21rem]">Perguntas frequentes</h3>
+              <h3 className="text-[3.5rem] text-vermelho-madeira font-belleza leading-none uppercase max-w-[21rem]">Perguntas frequentes</h3>
             </div>
 
         <div className="max-w-[700px]  bg-creme-claro grid-cols-2 justify-center mx-auto items-center">
 
             <Accordion className="bg-vermelho-madeira" open={open === 1} icon={<Icon id={1} open={open}  />}>
-              <AccordionHeader className="text-[2rem] pl-[2rem] text-creme font-belleza font-thin hover:text-creme" onClick={() => handleOpen(1)}>
+              <AccordionHeader className="text-[2rem] pl-[2rem] text-creme-claro font-extrabold font-mont hover:text-creme-claro" onClick={() => handleOpen(1)}>
                 Preciso fazer uma avaliação?
               </AccordionHeader>
-              <AccordionBody className="bg-creme text-[1.5rem] py-[2rem] px-[2rem] text-vermelho-madeira font-belleza font-thin leading-tight">
+              <AccordionBody className="bg-creme text-[1.5rem] py-[2rem] px-[2rem] text-vermelho-madeira font-mont font-semibold leading-tight">
                 Sim, pois cada cliente tem uma indicação e anatomia distinta. Os tratamentos variam
                 de acordo com o que vc deseja de resultado e indicação do profissional.
               </AccordionBody>
             </Accordion>
 
             <Accordion className="bg-vermelho-madeira mt-[2rem]" open={open === 2} icon={<Icon id={2} open={open}  />}>
-              <AccordionHeader className="text-[1.9rem] pl-[2rem]  text-creme font-belleza font-thin hover:text-creme" onClick={() => handleOpen(2)}>
+              <AccordionHeader className="text-[1.9rem] pl-[2rem]  text-creme-claro font-extrabold font-mont  hover:text-creme-claro" onClick={() => handleOpen(2)}>
                 Após procedimento o que devo ter de cuidados?
               </AccordionHeader>
-              <AccordionBody className="bg-creme text-[1.5rem] py-[2rem] px-[2rem] text-vermelho-madeira font-belleza font-thin leading-tight">
+              <AccordionBody className="bg-creme text-[1.5rem] py-[2rem] px-[2rem] text-vermelho-madeira font-mont font-semibold leading-tight">
                 Cada procedimento tem sua orientação. Mas pode ficar tranquila, rotina normal com alguns 
                 cuidados específicos de cada protocolo.
               </AccordionBody>
@@ -62,29 +62,29 @@ import whats from '../../assets/whats.svg'
           
             
             <Accordion className="bg-vermelho-madeira mt-[2rem]" open={open === 3} icon={<Icon id={3} open={open}  />}>
-              <AccordionHeader className="text-[1.9rem] pl-[2rem] text-creme font-belleza font-thin hover:text-creme" onClick={() => handleOpen(3)}>
+              <AccordionHeader className="text-[1.9rem] pl-[2rem] text-creme-claro font-extrabold font-mont hover:text-creme-claro" onClick={() => handleOpen(3)}>
                 Posso fazer mais de um procedimento no mesmo dia?
               </AccordionHeader>
-              <AccordionBody className="bg-creme text-[1.5rem] py-[2rem] px-[2rem] text-vermelho-madeira font-belleza font-thin leading-tight">
+              <AccordionBody className="bg-creme text-[1.5rem] py-[2rem] px-[2rem] text-vermelho-madeira font-mont font-semibold leading-tight">
                 Sim, porém tem protocolos que precisam de um intervalo entre eles para que sejam realizados.
               </AccordionBody>
             </Accordion>
 
             <Accordion className="bg-vermelho-madeira mt-[2rem]" open={open === 4} icon={<Icon id={4} open={open}  />}>
-              <AccordionHeader className="text-[2rem] pl-[2rem] text-creme font-belleza font-thin hover:text-creme" onClick={() => handleOpen(4)}>
+              <AccordionHeader className="text-[2rem] pl-[2rem] text-creme-claro font-extrabold font-mont hover:text-creme-claro" onClick={() => handleOpen(4)}>
               É feito com anestesia?
               </AccordionHeader>
-              <AccordionBody className="bg-creme text-[1.5rem] py-[2rem] px-[2rem] text-vermelho-madeira font-belleza font-thin leading-tight">
+              <AccordionBody className="bg-creme text-[1.5rem] py-[2rem] px-[2rem] text-vermelho-madeira font-mont font-semibold leading-tight">
                 Sim, nossos procedimentos são com anestesia tópica ou injetável, depende do protocolo.
               </AccordionBody>
             </Accordion>
 
             
             <Accordion className="bg-vermelho-madeira mt-[2rem]" open={open === 5} icon={<Icon id={5} open={open}  />}>
-              <AccordionHeader className="text-[2rem] pl-[2rem] text-creme font-belleza font-thin hover:text-creme" onClick={() => handleOpen(5)}>
+              <AccordionHeader className="text-[2rem] pl-[2rem] text-creme-claro font-extrabold font-mont  hover:text-creme-claro" onClick={() => handleOpen(5)}>
               Tem contraindicação?
               </AccordionHeader>
-              <AccordionBody className="bg-creme text-[1.5rem] py-[2rem] px-[2rem] text-vermelho-madeira font-belleza font-thin leading-tight">
+              <AccordionBody className="bg-creme text-[1.5rem] py-[2rem] px-[2rem] text-vermelho-madeira font-mont font-semibold leading-tight">
               Sim, principalmente lactantes e grávidas. É feito uma anamnese antes do seu procedimento 
                para entender um pouco da sua saúde e dar continuidade ao seu protocolo com toda 
               segurança.
@@ -93,10 +93,10 @@ import whats from '../../assets/whats.svg'
 
             
             <Accordion className="bg-vermelho-madeira mt-[2rem]" open={open === 6} icon={<Icon id={6} open={open}  />}>
-              <AccordionHeader className="text-[2rem] pl-[2rem] text-creme font-belleza font-thin hover:text-creme" onClick={() => handleOpen(6)}>
+              <AccordionHeader className="text-[2rem] pl-[2rem] text-creme-claro font-extrabold font-mont  hover:text-creme-claro" onClick={() => handleOpen(6)}>
               Resultado é imediato?
               </AccordionHeader>
-              <AccordionBody className="bg-creme text-[1.5rem] py-[2rem] px-[2rem] text-vermelho-madeira font-belleza font-thin leading-tight">
+              <AccordionBody className="bg-creme text-[1.5rem] py-[2rem] px-[2rem] text-vermelho-madeira font-mont font-semibold leading-tight">
               Alguns procedimentos sim, outros ao longo dos dias. Cada protocolo tem sua função e
               importância no seu tratamento.
               </AccordionBody>
@@ -104,10 +104,10 @@ import whats from '../../assets/whats.svg'
 
             
             <Accordion className="bg-vermelho-madeira mt-[2rem] " open={open === 7} icon={<Icon id={7} open={open}  />}>
-              <AccordionHeader className="text-[2rem] pl-[2rem] text-creme font-belleza font-thin hover:text-creme" onClick={() => handleOpen(7)}>
+              <AccordionHeader className="text-[2rem] pl-[2rem] text-creme-claro font-extrabold font-mont  hover:text-creme-claro" onClick={() => handleOpen(7)}>
               Esses procedimentos são compatíveis com o organismo?
               </AccordionHeader>
-              <AccordionBody className="bg-creme  text-[1.5rem] py-[2rem] px-[2rem] text-vermelho-madeira font-belleza font-thin leading-tight">
+              <AccordionBody className="bg-creme  text-[1.5rem] py-[2rem] px-[2rem] text-vermelho-madeira font-mont font-semibold leading-tight">
               São compatíveis e absorvidos pelo organismo. Melhor tecnologia que temos hoje na Estética é 
               usada na Salomão sempre pensando na sua segurança.
               </AccordionBody>
@@ -119,7 +119,8 @@ import whats from '../../assets/whats.svg'
 
           <a href='https://wa.me/5511997173432?text=Gostaria de saber mais sobre a Estética Salomão'>
 
-                    <button className='flex  justify-center items-center mx-auto bg-gradient-to-r from-whats-escuro via-whats to-whats-escuro border-4 border-branco drop-shadow-lg cursor-pointer px-[4rem] pt-[1rem] pb-[1rem] rounded-[8rem] hover:shadow-dentro hover:shadow-creme'>                        <span className='text-[2rem] font-belleza w-[43rem]  drop-shadow-lg text-branco leading-tight uppercase hover:text-[2.1rem] hover:w-[44rem]'>Quero iniciar agora meu tratamento</span>
+                    <button className='flex  justify-center items-center mx-auto bg-gradient-to-r from-whats-escuro via-whats to-whats-escuro border-4 border-branco drop-shadow-lg cursor-pointer px-[4rem] pt-[1rem] pb-[1rem] rounded-[8rem] hover:shadow-dentro hover:shadow-creme hover:transition-all hover:ease-out hover:duration-700'>
+                      <span className='text-[2rem] font-mont w-[46rem] font-extrabold drop-shadow-lg text-branco leading-tight uppercase hover:text-[2.1rem] hover:w-[49rem] hover:transition-all hover:ease-out hover:duration-700'>Quero iniciar agora meu tratamento</span>
                         <img src={whats} alt="" className='pl-[1rem]'/>
                     </button>
 
