9c109609894224d1cad94486e1dfe971206665a8
Guilherme da Rocha <guis.darocha@gmail.com>
2023-08-01 20:07:52 -0300

    Iniciado versão mobile header

--- clinica-salomao/src/components/Header/Header.tsx
+++ clinica-salomao/src/components/Header/Header.tsx
@@ -5,7 +5,7 @@ import imagemHeader from '../../assets/headerImage.png'
 const Header = () => {
     return (
     <>
-    <div className='bg-gradient-to-t from-creme-escuro to-creme w-full h-[1730px] pb-[5rem]'>
+    <div className='bg-gradient-to-t from-creme-escuro to-creme w-full h-[1500px] pb-[5rem]'>
         <div className="flex justify-center items-center mx-auto pt-6 bg-creme-claro h-[8rem] w-auto">
             <img src={logo} alt="" className='mx-auto max-w-[10rem] mt-[1rem] h-auto'/>
         </div>
@@ -20,8 +20,8 @@ const Header = () => {
 
 
         <div className="grid -mt-[3rem]">
-            <div className='flex mx-auto bg-siena content-center  '>
-                <div className=' h-auto w-[18rem] hover:shadow-dentro hover:shadow-creme hover:transition-all hover:ease-out hover:duration-700 rounded-tl-46 flex justify-left items-center'>
+            <div className='flex mx-auto bg-siena rounded-tl-6xl content-center   '>
+                <div className=' h-auto w-[18rem] py-[3rem] px-[0.5rem] hover:shadow-dentro hover:shadow-creme hover:transition-all hover:ease-out hover:duration-700 rounded-tl-46 flex justify-left items-center'>
                     <h2 className='text-[1.5rem] font-mont text-creme leading-tight text-center'>Você que tem entre 40 e 65 anos, que tal 
                     <span className='text-[1.5rem] font-extrabold text-creme'> ELIMINAR</span> a
                     <span className='text-[1.5rem] font-extrabold text-creme'> FLACIDEZ</span> e
@@ -30,15 +30,15 @@ const Header = () => {
                 </div>
             </div>          
             <div className="">
-                <img src={imagemHeader} alt="" className=' ' />
+                <img src={imagemHeader} alt="" className=' -mt-[2rem] ' />
             </div>
        </div>
 
         
         
-        <div className="flex items-center mx-auto drop-shadow-lg  max-w-[1180px] ">
+        <div className="flex items-center mx-auto drop-shadow-lg ">
             <div className="bg-vermelho-madeira rounded-tl-46 rounded-br-46 ">
-                <h3 className='text-[2.5rem] text-center font-mont text-creme leading-tight  py-[3rem] px-[4rem] '>Tratamentos que estimulam colágeno de forma natural e muito segura, proporcionando um resultado maravilhoso devolvendo sua autoestima com segurança</h3>
+                <h3 className='text-[1.5rem] text-center font-mont text-creme leading-tight  py-[3rem] px-[4rem] '>Tratamentos que estimulam colágeno de forma natural e muito segura, proporcionando um resultado maravilhoso devolvendo sua autoestima com segurança</h3>
             </div>
         </div>
     </div>
--- clinica-salomao/tailwind.config.js
+++ clinica-salomao/tailwind.config.js
@@ -33,6 +33,7 @@ module.exports = withMT({
     },
     borderRadius: {
       '46': '80px',
+      '6xl': '48px',
     },
 
     boxShadow: {
