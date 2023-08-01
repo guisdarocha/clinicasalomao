e7e9326e4aa94e489bcea7853f6af8d312e9f50f
Guilherme da Rocha <guis.darocha@gmail.com>
2023-08-01 19:36:56 -0300

    Iniciado versão mobile header

--- clinica-salomao/src/components/Header/Header.tsx
+++ clinica-salomao/src/components/Header/Header.tsx
@@ -5,38 +5,38 @@ import imagemHeader from '../../assets/headerImage.png'
 const Header = () => {
     return (
     <>
-    <div className='bg-gradient-to-t from-creme-escuro to-creme w-full h-[1730px] xl:w-auto md:bg-creme  sm:bg-creme '>
-        <div className="flex justify-center pt-6 bg-creme-claro h-[11.37rem] w-auto">
-            <img src={logo} alt="" className='absolute max-w-[22rem] h-auto'/>
+    <div className='bg-gradient-to-t from-creme-escuro to-creme w-full h-[1730px] pb-[5rem]'>
+        <div className="flex justify-center items-center mx-auto pt-6 bg-creme-claro h-[8rem] w-auto">
+            <img src={logo} alt="" className='mx-auto max-w-[10rem] mt-[1rem] h-auto'/>
         </div>
 
 
 
         <div className="grid justify-items-center pt-20 pb-20">
-            <h1 className='text-[6.875rem] -mb-12 font-belleza text-vermelho-madeira uppercase'>Rejuvenescer</h1>
-            <h1 className='text-[6.5rem] -mb-4 font-belleza text-vermelho-madeira uppercase'>mais de <span className='text-[9rem] text-siena'>10</span> anos </h1>
-            <h1 className='text-[4rem] font-belleza text-vermelho-madeira uppercase'>sem cirurgia é possível</h1>
+            <h1 className='text-[2.5rem] -mb-4 font-belleza text-vermelho-madeira uppercase'>Rejuvenescer</h1>
+            <h1 className='text-[2.5rem]  -mb-2  font-belleza text-vermelho-madeira uppercase'>mais de <span className='text-[4rem] text-siena'>10</span> anos </h1>
+            <h1 className='text-[1.5rem] font-belleza text-vermelho-madeira uppercase'>sem cirurgia é possível</h1>
         </div>
 
 
-        <div className="flex justify-center -mt-[3rem]">
-            <div className='flex content-center pt-[12rem] pl-[5rem] pb-[16rem] md:pb-[32rem] h-auto '>
-                <div className='bg-siena  w-[55rem] h-auto hover:shadow-dentro hover:shadow-creme hover:transition-all hover:ease-out hover:duration-700 rounded-tl-46 flex justify-left items-center  pl-[4rem] py-[3rem] '>
-                    <h2 className='text-[3.12rem]  max-w-[23rem] font-mont text-creme leading-tight text-left'>Você que tem entre 40 e 65 anos, que tal 
-                    <span className='text-[3.12rem] font-extrabold text-creme'> ELIMINAR</span> a
-                    <span className='text-[3.12rem] font-extrabold text-creme'> FLACIDEZ</span> e
-                    as <span className='text-[3.12rem] font-extrabold text-creme'> RUGAS</span> 
+        <div className="grid -mt-[3rem]">
+            <div className='flex mx-auto bg-siena content-center  '>
+                <div className=' h-auto w-[18rem] hover:shadow-dentro hover:shadow-creme hover:transition-all hover:ease-out hover:duration-700 rounded-tl-46 flex justify-left items-center'>
+                    <h2 className='text-[1.5rem] font-mont text-creme leading-tight text-center'>Você que tem entre 40 e 65 anos, que tal 
+                    <span className='text-[1.5rem] font-extrabold text-creme'> ELIMINAR</span> a
+                    <span className='text-[1.5rem] font-extrabold text-creme'> FLACIDEZ</span> e
+                    as <span className='text-[1.5rem] font-extrabold text-creme'> RUGAS</span> 
                     </h2>
                 </div>
             </div>          
-            <div className="absolute right-1 max-w-[35rem] max-h-[81rem] xl:right-[3rem] 2xl:right-[12rem]">
+            <div className="">
                 <img src={imagemHeader} alt="" className=' ' />
             </div>
        </div>
 
         
         
-        <div className="flex items-center mx-auto drop-shadow-lg -mt-[16rem] max-w-[1180px] ">
+        <div className="flex items-center mx-auto drop-shadow-lg  max-w-[1180px] ">
             <div className="bg-vermelho-madeira rounded-tl-46 rounded-br-46 ">
                 <h3 className='text-[2.5rem] text-center font-mont text-creme leading-tight  py-[3rem] px-[4rem] '>Tratamentos que estimulam colágeno de forma natural e muito segura, proporcionando um resultado maravilhoso devolvendo sua autoestima com segurança</h3>
             </div>
