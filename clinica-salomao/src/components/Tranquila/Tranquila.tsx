155685c902f6e2a6940bb2ac7acc94fa341fd3d6
Guilherme da Rocha <guis.darocha@gmail.com>
2023-08-05 08:34:14 -0300

    iniciado responsividade mobile

--- clinica-salomao/src/components/Tranquila/Tranquila.tsx
+++ clinica-salomao/src/components/Tranquila/Tranquila.tsx
@@ -2,28 +2,28 @@
 const Tranquila = () => {
     return (
     <>
-    <div className=" bg-laranja flex justify-center items-center mx-auto h-[1200px]">
+    <div className=" bg-laranja flex justify-center items-center h-[600px] rounded-br-2xl">
 
-        <div className="grid grid-cols-1 max-w-[1080px]">
-            <h3 className='text-[3.9rem] w-[55rem] text-center text-creme font-belleza leading-tight justify-center items-center mx-auto uppercase'>Pode ficar tranquila, estética não tem idade.</h3>
-            <h4 className='text-[2rem] pt-[2rem] text-creme font-mont font-semibold justify-center items-center mx-auto'>Vamos lá, faça seu procedimento mesmo que...</h4>
+        <div className="grid grid-cols-1 mt-4 max-w-[360px]">
+            <h3 className='text-[1.5rem] text-center text-creme font-belleza leading-tight justify-center items-center mx-auto uppercase'>Pode ficar tranquila, estética não tem idade.</h3>
+            <h4 className='text-[1rem] pt-[1rem] text-creme w-[17.1rem] text-center font-mont font-semibold justify-center leading-tight items-center mx-auto'>Vamos lá, faça seu procedimento mesmo que...</h4>
             
-            <div className='justify-center gap-3 items-center mx-auto pt-[2rem] '>
-                <div className='flex justify-center text-center items-center max-w-[55rem]  px-[4rem] py-[1rem] rounded-tl-46 rounded-br-46 bg-vermelho-madeira'>
-                    <h5 className='text-[1.8rem] w-[29rem] font-mont text-creme'>PREVENTIVAMENTE, e dê um breque no seu envelhecimento</h5>
+            <div className='justify-center items-center mx-auto pt-[2rem] '>
+                <div className='flex justify-center text-center items-center  my-2 px-[4rem] py-[1rem] rounded-tl-46 rounded-br-46 bg-vermelho-madeira'>
+                    <h5 className='text-[0.87rem] font-mont text-creme'>PREVENTIVAMENTE, e dê um breque no seu envelhecimento</h5>
                 </div>
-                <div className='flex justify-center text-center items-center mt-[2rem] max-w-[48rem] px-[4rem] py-[1rem] rounded-tl-46 rounded-br-46 bg-vermelho-madeira'>
-                    <h5 className='text-[1.8rem] w-[29rem] font-mont text-creme'>Corretivamente, e rejuvenesça muitos anos</h5>
+                <div className='flex justify-center text-center items-center  my-2 px-[4rem] py-[1rem] rounded-tl-46 rounded-br-46 bg-vermelho-madeira'>
+                    <h5 className='text-[0.87rem] font-mont text-creme'>Corretivamente, e rejuvenesça muitos anos</h5>
                 </div>
-                <div className='flex justify-center text-center items-center mt-[2rem] max-w-[48rem] px-[4rem] py-[1rem] rounded-tl-46 rounded-br-46 bg-vermelho-madeira'>
-                    <h5 className='text-[1.8rem] w-[29rem] font-mont text-creme' >Mesmo nunca tendo feito nada</h5>
+                <div className='flex justify-center text-center items-center  my-2 px-[4rem] py-[1rem] rounded-tl-46 rounded-br-46 bg-vermelho-madeira'>
+                    <h5 className='text-[0.87rem] font-mont text-creme' >Mesmo nunca tendo feito nada</h5>
                 </div>
-                <div className='flex justify-center text-center items-centermt-[2rem] mt-[2rem] max-w-[48rem] px-[4rem] py-[1rem] rounded-tl-46 rounded-br-46 bg-vermelho-madeira'>
-                    <h5 className='text-[1.8rem] w-[29rem] font-mont text-creme' >Independente da sua idade</h5>
+                <div className='flex justify-center text-center items-center my-2 px-[4rem] py-[1rem] rounded-tl-46 rounded-br-46 bg-vermelho-madeira'>
+                    <h5 className='text-[0.87rem] font-mont text-creme' >Independente da sua idade</h5>
                 </div>
             </div>
 
-            <h3 className='text-[3.9rem] w-[55rem] mt-[4rem] text-center text-creme font-belleza leading-tight justify-center items-center mx-auto uppercase'>Só depende de você</h3>
+            <h3 className='text-[1.5rem]  mt-[2rem] text-center text-creme font-belleza leading-tight justify-center items-center mx-auto uppercase'>Só depende de você</h3>
             
 
         </div>
