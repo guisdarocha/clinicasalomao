2b6d7cc80cdf7bb4ec92cda622b91a1020fcf556
Guilherme da Rocha <guis.darocha@gmail.com>
2023-08-07 21:34:29 -0300

    Iniciada revisão da pagina

--- clinica-salomao/src/components/Slider.tsx
+++ clinica-salomao/src/components/Slider.tsx
@@ -5,51 +5,28 @@ import dois from '../assets/mockupDois.jpeg'
 export function Celular() {
   return (
     <Carousel
-      className="absolute left-[12.3rem] mt-[13rem] "
+    
+      className="absolute left-[12.3rem] mt-[13rem] rounded-xl"
       navigation={({ setActiveIndex, activeIndex, length }) => (
         <div className="hidden">
           {new Array(length).fill("").map((_, i) => (
             <span
               key={i}
               className={`block h-1 cursor-pointer rounded-2xl transition-all content-[''] ${
-                activeIndex === i ? "w-8 bg-white" : "w-4 bg-white/50"
+                activeIndex === i ? "w-10 bg-white" : "w-4 bg-white/50"
               }`}
               onClick={() => setActiveIndex(i)}
             />
           ))}
         </div>
       )}
-      prevArrow={({ handlePrev }) => (
+      nextArrow={({ handleNext }) => (
         <IconButton
           variant="text"
           color="white"
           size="lg"
-          onClick={handlePrev}
-          className=""
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={2}
-            stroke="currentColor"
-            className="h-6 w-6"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M10.5 19.5L3 12m0 0l7.5-7.5M3 12h18"
-            />
-          </svg>
-        </IconButton>
-      )}
-      nextArrow={({ handleNext }) => (
-        <IconButton
-          variant="text"
-          color="red"
-          size="lg"
           onClick={handleNext}
-          className=""
+          className="!absolute bg-vermelho-madeira px-[3rem] right-[18rem]"
         >
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -84,5 +61,6 @@ export function Celular() {
         className="w-[128px] h-[280px] rounded-xl"
       />
     </Carousel>
+    
   );
 }
