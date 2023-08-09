64d3a0d5653db2f748148050aea9c084c305719d
Guilherme da Rocha <guis.darocha@gmail.com>
2023-08-09 19:26:28 -0300

    Finalizando faq

--- clinica-salomao/src/components/Perguntas/Acordeao.tsx
+++ clinica-salomao/src/components/Perguntas/Acordeao.tsx
@@ -25,13 +25,14 @@ const AcordeaoSection = ({section, isActiveSection, sectionIndex, setActiveIndex
         const nextIndex = isActiveSection ? null : sectionIndex;
         setActiveIndex(nextIndex);
     }
+
     return (
         <div className='mt-[3rem] pt-[1.5rem] -my-[3rem]'>
             <div onClick={toggleSection} className='flex bg-gradient-to-b from-perguntaA to-perguntaB px-[1rem] py-[0.5rem] rounded-46 drop-shadow-md  '>
                 <div className='flex ml-2 justify-start items-center font-mont font-bold text-white text-[0.8rem] w-[13rem] leading-tight'>{section.title}</div>
-                <div className='flex items-center ml-6 max-w-[1.5rem] '>{isActiveSection ? <img src={setaUp} alt="-"  /> : <img src={setaDown} alt="+" />}</div>
+                <div className=' flex items-center ml-6 max-w-[1.5rem] transition-all ease-out duration-700 '>{isActiveSection ? <img src={setaUp} alt="-"  /> : <img src={setaDown} alt="+" />}</div>
             </div>
-            {isActiveSection && <div className='bg-creme flex mx-auto px-[1rem] py-[1rem] drop-shadow-xl font-mont font-medium rounded-xl text-vermelho-madeira leading-tight text-[0.8rem] transition-all ease-out duration-700'>{section.text}</div>}
+            {isActiveSection && <div className='relative mt-[0.1rem] bg-creme flex mx-auto px-[1rem] py-[1rem] drop-shadow-xl font-mont font-medium rounded-xl text-vermelho-madeira leading-tight text-[0.8rem] transition-all ease-out duration-700'>{section.text}</div>}
         </div>
     );
 };
--- clinica-salomao/src/components/Perguntas/Perguntas.tsx
+++ clinica-salomao/src/components/Perguntas/Perguntas.tsx
@@ -7,26 +7,29 @@ import botao from '../../assets/botao.png'
   
   return (
     <>
-        <div className="bg-creme-claro  pt-[5rem]">
-            <div className="w-[5rem] flex justify-start px-[2rem]">
+    <div className="bg-creme-claro">
+
+        <div className=" justify-center max-w-[360px] mx-auto pt-[3rem]">
+            <div className="w-[5rem] flex justify-start px-[2rem] -mb-[3rem]">
               <h3 className="text-[1.5rem] text-vermelho-madeira font-belleza leading-none uppercase">Perguntas frequentes</h3>
             </div>
       
-        <div>
+        <div className="transition-all ease-out duration-700">
           <Faq />
         </div>
 
-        <div className='col-span-3 flex justify-center pt-[4rem] pb-[4rem]'>
+        <div className='col-span-3 flex justify-center pt-[6rem] pb-[4rem]'>
 
                     <a href='https://wa.me/5511997173432?text=Gostaria de saber mais sobre a Estética Salomão'>
                         <button className='flex justify-center items-center mx-auto cursor-pointer drop-shadow-lg '>
-                            <img src={botao} alt="" className='relative w-[20rem] hover:w-[22rem] hover:shadow-dentro hover:shadow-creme hover:transition-all hover:ease-out hover:duration-700'/>
-                            <span className='absolute pr-[1rem] text-[0.8rem] font-mont font-extrabold drop-shadow-lg leading-tight text-branco uppercase'>Quero tratar minha flacidez</span>
-                            <img src={whats} alt="" className='absolute right-6 w-[1.5rem] h-auto self-center -mr-3'/>
+                            <img src={botao} alt="" className='relative w-[22rem] hover:w-[22rem] hover:shadow-dentro hover:shadow-creme hover:transition-all hover:ease-out hover:duration-700'/>
+                            <span className='absolute pr-[1rem] text-[0.8rem] font-mont font-extrabold drop-shadow-lg leading-tight text-branco uppercase'>Quero iniciar agora meu tratamento</span>
+                            <img src={whats} alt="" className='absolute right-6 w-[1.5rem] h-auto self-center -mr-4'/>
                         </button>
                     </a>  
             </div>
       </div>
+    </div>
      
     </>
   );
