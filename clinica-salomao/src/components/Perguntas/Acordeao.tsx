698c7a341cd9f867e3a34a18093ed66b3618af2f
Guilherme da Rocha <guis.darocha@gmail.com>
2023-09-02 10:13:07 -0300

    Adicionado desktop componente faq

--- clinica-salomao/src/components/Perguntas/Acordeao.tsx
+++ clinica-salomao/src/components/Perguntas/Acordeao.tsx
@@ -28,13 +28,13 @@ const AcordeaoSection = ({section, isActiveSection, sectionIndex, setActiveIndex
     
 
     return (
-        <div className='mt-[3rem] pt-[1.5rem] -my-[3rem]'>
+        <div className='mt-[3rem] pt-[1.5rem] -my-[3rem] xl:mt-[10rem] xl:my-[-8rem]'>
             
             <div onClick={toggleSection} className='flex bg-gradient-to-b from-perguntaA to-perguntaB px-[1rem] py-[0.5rem] rounded-46 drop-shadow-md  '>
-                <div className='flex ml-2 justify-start items-center font-mont font-bold text-white text-[0.8rem] w-[13rem] leading-tight'>{section.title}</div>
-                <div className=' flex items-center ml-6 max-w-[1.5rem] transition-all ease-out duration-700 '>{isActiveSection ? <img src={setaUp} alt="-"  /> : <img src={setaDown} alt="+" />}</div>
+                <div className='flex ml-2 justify-start items-center font-mont font-bold text-white text-[0.8rem] w-[13rem] leading-tight xl:text-[2rem] xl:w-[35rem] xl:pl-4 xl:py-2'>{section.title}</div>
+                <div className=' flex items-center ml-6 max-w-[1.5rem] transition-all ease-out duration-700 xl:ml-[18rem] xl:max-w-[1.5rem]'>{isActiveSection ? <img src={setaUp} alt="-"  /> : <img src={setaDown} alt="+" />}</div>
             </div>
-            {isActiveSection && <div className='mt-[0.1rem] bg-creme flex mx-auto px-[1rem] py-[1rem] drop-shadow-xl font-mont font-medium rounded-xl shadow-inner text-vermelho-madeira leading-tight text-[0.8rem]'>{section.text}</div>}
+            {isActiveSection && <div className='mt-[0.1rem] bg-creme flex mx-auto px-[1rem] py-[1rem] drop-shadow-xl font-mont font-medium rounded-xl shadow-inner text-vermelho-madeira leading-tight text-[0.8rem] xl:text-[1.8rem] xl:py-8 xl:px-7'>{section.text}</div>}
         </div>
     );
 };
@@ -43,7 +43,7 @@ const AcordeaoSection = ({section, isActiveSection, sectionIndex, setActiveIndex
 const Acordeao = ({ sections } : AcordeaoProps) => {
     const [activeIndex, setActiveIndex] = useState(0);
 return (
-    <div className="bg-creme-claro w-[360px] px-[2rem]">
+    <div className="bg-creme-claro w-[360px] px-[2rem] xl:w-[1000px]">
         {sections.map((section: any, index: Key | null | undefined) => (
         <AcordeaoSection section={section} key={index} 
         isActiveSection={index === activeIndex} 
--- clinica-salomao/src/components/Perguntas/Perguntas.tsx
+++ clinica-salomao/src/components/Perguntas/Perguntas.tsx
@@ -1,6 +1,6 @@
 import Faq from "./Faq";
 import whats from '../../assets/whats.svg'
-import botao from '../../assets/botao.png'
+import botao from '../../assets/botaowhats.svg'
 
  const Perguntas = () => {
 
@@ -9,26 +9,26 @@ import botao from '../../assets/botao.png'
     <>
     <div className="bg-creme-claro">
 
-        <div className=" justify-center max-w-[360px] mx-auto pt-[3rem]">
-            <div className="w-[5rem] flex justify-start px-[2rem] -mb-[3rem]">
-              <h3 className="text-[1.5rem] text-vermelho-madeira font-belleza leading-none uppercase">Perguntas frequentes</h3>
+        <div className=" justify-center max-w-[360px] mx-auto pt-[3rem] xl:max-w-[1280px] xl:h-[80rem] xl:flex xl:mx-auto xl:justify-center">
+            <div className="w-[5rem] flex justify-start px-[2rem] -mb-[3rem] ">
+              <h3 className="text-[1.5rem] text-vermelho-madeira font-belleza leading-none uppercase xl:text-[4.3rem] xl:ml-[5rem]">Perguntas frequentes</h3>
             </div>
       
         <div className="transition-all ease-out duration-700">
           <Faq />
         </div>
 
-        <div className='col-span-3 flex justify-center pt-[6rem] pb-[4rem]'>
 
-                    <a href='https://wa.me/5511997173432?text=Gostaria de saber mais sobre a Estética Salomão'>
-                        <button className='flex justify-center items-center mx-auto cursor-pointer drop-shadow-lg '>
-                            <img src={botao} alt="" className='relative w-[22rem] hover:w-[22rem] hover:shadow-dentro hover:shadow-creme hover:transition-all hover:ease-out hover:duration-700'/>
-                            <span className='absolute pr-[1rem] text-[0.8rem] font-mont font-extrabold drop-shadow-lg leading-tight text-branco uppercase'>Quero iniciar agora meu tratamento</span>
-                            <img src={whats} alt="" className='absolute right-6 w-[1.5rem] h-auto self-center -mr-4'/>
-                        </button>
-                    </a>  
-            </div>
       </div>
+      <div className='flex justify-center items-center pt-[2rem] xl:mx-auto xl:pb-[5rem] '>
+            <a href='https://wa.me/5511997173432?text=Gostaria de saber mais sobre a Estética Salomão'>
+                <button className='flex justify-center items-center mx-auto cursor-pointer drop-shadow-lg '>
+                    <img src={botao} alt="" className='relative w-[21rem]  xl:w-[70rem] hover:w-[22rem] hover:shadow-dentro hover:shadow-creme hover:transition-all hover:ease-out hover:duration-700 xl:hover:w-[72rem] xl:hover:shadow-dentro xl:hover:shadow-creme xl:hover:transition-all xl:hover:ease-out xl:hover:duration-700'/>
+                    <span className='absolute pr-[1rem] text-[0.8rem] font-mont font-extrabold drop-shadow-lg leading-tight text-branco uppercase xl:text-[2.6rem] xl:pr-[5rem]'>Quero iniciar agora meu tratamento</span>
+                    <img src={whats} alt="" className='absolute right-6 w-[1.5rem] h-auto  self-center xl:w-[5rem] '/>
+                </button>
+            </a>  
+        </div>
     </div>
      
     </>
