c62fc8e01ce90a3a95d61d37de9a7be967eef0b9
Guilherme da Rocha <guis.darocha@gmail.com>
2023-09-02 10:03:59 -0300

    Adicionado desktop componente depoimentos

--- clinica-salomao/src/components/Depoimento.tsx/Depoimento.tsx
+++ clinica-salomao/src/components/Depoimento.tsx/Depoimento.tsx
@@ -12,7 +12,7 @@ const Depoimento = () => {
         <div className="grid grid-cols-2 max-w-[360px] py-[rem] xl:max-w-[1280px] ">
             <div className=" font-belleza text-creme ml-[2rem] w-[11rem] mt-[4rem] pr-6 xl:ml-[-10rem]">
                 <h3 className='text-[1.125rem] leading-tight uppercase xl:text-[3.1rem] xl:w-[33rem]'>Veja o que nossos clientes relatam sobre nós</h3>
-                <h5 className='text-[0.875rem] mt-[1rem] font-mont leading-tight xl:text-[2.1rem] xl:w-[26rem] xl:pb-[5rem]'>Passe para o lado e veja outras avaliações</h5>
+                <h5 className='text-[0.875rem] mt-[1rem] font-mont leading-tight xl:text-[2.1rem] xl:w-[26rem] xl:pb-[10rem]'>Passe para o lado e veja outras avaliações</h5>
                 <img src={setaCelular} alt="" className='mt-3 xl:w-[16rem]' />
             </div>      
             <img src={celular} alt="" className='w-[145px] ml-[0.5rem] xl:w-[21rem]' />
